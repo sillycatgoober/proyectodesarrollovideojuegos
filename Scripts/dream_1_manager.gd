729337179f58439c797f3d854516f1719219e1d8
LaEscobarGit <nataliaescobarhdez@gmail.com>
2026-06-25 13:25:53 -0700

    Ajustes del merge
    Ajustes del merge

--- Scripts/dream_1_manager.gd
+++ Scripts/dream_1_manager.gd
@@ -43,4 +43,5 @@ func _on_carta_agregada(item_id):
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.name == "Player":
+		print("salio")
 		#TransitionManager.play_transition("res://Scenes/Interview/interview.tscn")
