4db3209d4a638252e553d0c353038cd926a359f6
LaEscobarGit <nataliaescobarhdez@gmail.com>
2026-06-25 16:16:08 -0700

    Commit Peque;o

--- Scripts/Interactuables/interactuable.gd
+++ Scripts/Interactuables/interactuable.gd
@@ -5,11 +5,13 @@ class_name Interactuable
 @export var puede_interactuar:=true
 @export var focus_offset := Vector3(0, 1.5, -0.5)
 @onready var camara:Camera3D
-@onready var audio = $AudioStreamPlayer
+@onready var audio:AudioStreamPlayer
 var jugador_cerca := false
 var en_interaccion := false
 
 func _ready() -> void:
+	if $AudioStreamPlayer!=null:
+		audio = $AudioStreamPlayer
 	if usa_focus:
 		camara = $Camera3D
 
